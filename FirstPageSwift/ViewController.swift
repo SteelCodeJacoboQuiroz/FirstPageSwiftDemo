3a7704fcc2bf782abe378ef798b21003e42d9626
GustavoOrtega <ovatsug_co@hotmail.com>
2019-07-10 19:01:02 -0500

    Initial Commit

---
+++ FirstPageSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstPageSwift
+//
+//  Created by Pruebas Mobiik on 7/10/19.
+//  Copyright © 2019 Pruebas Mobiik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
